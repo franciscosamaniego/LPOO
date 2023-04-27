80ad77c42854c63fbc295be1588b165f14f727c4
franciscosamaniego <102254119+franciscosamaniego@users.noreply.github.com>
2023-04-27 10:04:35 -0300

    Update FranciscoSamaniegoConjetura.cpp

--- FranciscoSamaniegoConjetura.cpp
+++ FranciscoSamaniegoConjetura.cpp
@@ -8,31 +8,16 @@ int main(void) {
         printf("%d\n", n);
         return 0;
     }
-    else if((n % 2) == 0) {
         while(n != 1) { //si empieza siendo par, se lo divide por dos y si queda impar, se lo multiplica por
         //3 y se le suma 1 y lo va imprimiendo
             if(n % 2 == 0) {
-                printf("%d ", n / 2);
+                printf("%d ", n);
                 n /= 2;
             }
             else {
-                printf("%d ", n * 3 + 1);
+                printf("%d ", n);
                 n = n * 3 + 1;
             }
         }
-    }
-    else {
-        while(n != 1) {
-            if(n % 2 == 0) { //si empieza siendo impar, primero multiplica por 3 y le suma 1 y luego si es par
-            //lo divide por 2 y lo va imprimendo
-                printf("%d ", n / 2);
-                n /= 2;
-            }
-            else {
-                printf("%d ", n * 3 + 1);
-                n = n * 3 + 1;
-            }
-        }
-    }
     return 0;
 }
